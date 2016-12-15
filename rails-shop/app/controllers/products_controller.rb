e2d39db6fd21bf412f5b201ab715d54b87f199c6
Kingsley Ijomah <kijomah@rawnet.com>
2016-12-15 17:53:53 +0000

    use spaces

--- rails-shop/app/controllers/products_controller.rb
+++ rails-shop/app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-	def index
-		@products = Product.all
-	end
+  def index
+    @products = Product.all
+  end
 end
--- rails-shop/config/routes.rb
+++ rails-shop/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-	root to: 'products#index'
+  root to: 'products#index'
 
   namespace :admin do
-  	root to: 'products#index'
-  	resources :products
+    root to: 'products#index'
+    resources :products
   end
 
   resources :products, only: [:index]
